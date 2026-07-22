14f308f2a6f1ef614b72db53dd6ebb168e21b20a
andre <andre@MSI>
2026-07-21 22:52:57 -0400

    se borro todo pero aqui esta

---
+++ ProyectoFinalPOO/src/logico/Tecnico.java
@@ -0,0 +1,44 @@
+package logico;
+
+import java.util.ArrayList;
+
+public class Tecnico extends Persona {
+	private String instituto;
+	private String diplomaTecnico;
+	private String especialidad;
+	
+	public Tecnico(String id, String cedula, String nombre, String telefono, String correo, boolean empleado,
+			ArrayList<Solicitud> solicitudes, String tiempoDisponible, float salarioMin, float salarioMax,
+			boolean tieneLicencia, char sexo, boolean disMudar, String ciudad, int annosExperiencia, String instituto,
+			String diplomaTecnico, String especialidad) {
+		super(id, cedula, nombre, telefono, correo, empleado, solicitudes, tiempoDisponible, salarioMin, salarioMax,
+				tieneLicencia, sexo, disMudar, ciudad, annosExperiencia);
+		this.instituto = instituto;
+		this.diplomaTecnico = diplomaTecnico;
+		this.especialidad = especialidad;
+	}
+
+	public String getInstituto() {
+		return instituto;
+	}
+
+	public void setInstituto(String instituto) {
+		this.instituto = instituto;
+	}
+
+	public String getDiplomaTecnico() {
+		return diplomaTecnico;
+	}
+
+	public void setDiplomaTecnico(String diplomaTecnico) {
+		this.diplomaTecnico = diplomaTecnico;
+	}
+
+	public String getEspecialidad() {
+		return especialidad;
+	}
+
+	public void setEspecialidad(String especialidad) {
+		this.especialidad = especialidad;
+	}
+}
---
+++ ProyectoFinalPOO/src/logico/Trabajador.java
@@ -0,0 +1,23 @@
+package logico;
+
+import java.util.ArrayList;
+
+public class Trabajador extends Persona {
+	private String oficio;
+
+	public Trabajador(String id, String cedula, String nombre, String telefono, String correo, boolean empleado,
+			ArrayList<Solicitud> solicitudes, String tiempoDisponible, float salarioMin, float salarioMax,
+			boolean tieneLicencia, char sexo, boolean disMudar, String ciudad, int annosExperiencia, String oficio) {
+		super(id, cedula, nombre, telefono, correo, empleado, solicitudes, tiempoDisponible, salarioMin, salarioMax,
+				tieneLicencia, sexo, disMudar, ciudad, annosExperiencia);
+		this.oficio = oficio;
+	}
+
+	public String getOficio() {
+		return oficio;
+	}
+
+	public void setOficio(String oficio) {
+		this.oficio = oficio;
+	}
+}
