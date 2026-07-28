1a56569bbf5f2c67a9b4c743799ce7d6b63ffd92
Manuelle28 <manuellemencia@gmail.com>
2026-07-28 13:53:32 -0400

    Se agregó el poder hacer backup de los archivos a través de un socket.

---
+++ src/logico/ClienteBackup.java
@@ -0,0 +1,53 @@
+package logico;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClienteBackup {
+	
+	public static void enviarRespaldo(String[] listaArchivos) {
+		
+		try (Socket sckCliente = new Socket("127.0.0.1",7000);
+			DataOutputStream salida = new DataOutputStream(sckCliente.getOutputStream())) {
+			
+			for (String rutaArchivo : listaArchivos) {
+				File archivo = new File(rutaArchivo);
+				
+				if(archivo.exists() == true) {
+					try (FileInputStream entradaArchivo = new FileInputStream(archivo)){
+						salida.writeUTF(archivo.getName());
+						salida.writeLong(archivo.length());
+			            salida.flush();
+			            
+			            long sizeArchivo = archivo.length();
+			            long sizeActual = 0;
+			            int unByte = entradaArchivo.read();
+			            
+			            while(sizeActual < sizeArchivo && unByte != -1) {
+			            	salida.write(unByte);
+			            	sizeActual++;
+			            	unByte = entradaArchivo.read();
+			            }
+			            salida.flush();
+			            System.out.println("Respaldo: "+archivo.getName()+" ha sido guardado");
+					}catch (IOException e) {
+						System.out.println("Error al leer el archivo: "+archivo.getName());
+					}
+				}else {
+					System.out.println("El archivo: "+rutaArchivo+" no existe");
+				}
+			}
+			
+		}catch(IOException e) {
+			System.out.println("Error de conexión");
+		}
+	}
+	
+	public static void main(String[] args) {
+		String[] losArchivos = {"IdGenerador.dat","Usuraio.dat","Personas.dat","Empresa.dat","Ofertas.dat","Solicitudes.dat"};
+		enviarRespaldo(losArchivos);
+	}
+}
---
+++ src/logico/ServidorBackup.java
@@ -0,0 +1,63 @@
+package logico;
+
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.LocalDate;
+
+public class ServidorBackup extends Thread {
+
+	private static String respaldos = "Respaldos" + File.separator;
+	
+	public static void main (String args[]) {
+		
+		File carpeta = new File(respaldos);
+		if(carpeta.exists() == false) {
+			carpeta.mkdirs();
+		}
+		
+		ServerSocket sckServer = null;
+		try {
+			sckServer = new ServerSocket(7000);
+		}catch(IOException e) {
+			System.out.println("Comunicación rechazada.");
+			System.exit(1);
+		}
+		
+		while(true) {
+			try (Socket sckClient = sckServer.accept();
+				DataInputStream entrada = new DataInputStream(sckClient.getInputStream())){
+				
+				boolean pasandoArchivos = true;
+				
+				while(pasandoArchivos) {
+					try {
+						String nombreArchivo = entrada.readUTF();
+						long sizeArchivo = entrada.readLong();
+						File archivoSalida = new File(respaldos+"Respaldo_"+LocalDate.now()+"_"+nombreArchivo);
+						
+						try (FileOutputStream salida = new FileOutputStream(archivoSalida)){
+		                	int unByte = entrada.read();
+		                	long sizeActual = 0;
+		                	while(sizeActual < sizeArchivo && unByte != -1) {
+		                		salida.write(unByte);
+		                		sizeActual++;
+		                		unByte = entrada.read();
+		                		
+		                	}
+		                	System.out.println("Respaldo guardado: "+archivoSalida.getName());
+		                }
+					}catch (EOFException e) {
+						pasandoArchivos = false;
+					}
+				}
+			}catch(IOException e) {
+				System.out.println("Error");
+			}
+		}
+	}
+}
