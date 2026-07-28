7aacdd160653b6c9e2bb9440e33ade79a7faf594
Manuelle28 <manuellemencia@gmail.com>
2026-07-28 18:48:50 -0400

    Los backups ahora funcionan en varios hilos.

--- src/logico/ServidorBackup.java
+++ src/logico/ServidorBackup.java
@@ -29,32 +29,42 @@ public class ServidorBackup extends Thread {
 		}
 		
 		while(true) {
-			try (Socket sckClient = sckServer.accept();
-				DataInputStream entrada = new DataInputStream(sckClient.getInputStream())){
-				
-				boolean pasandoArchivos = true;
-				
-				while(pasandoArchivos) {
-					try {
-						String nombreArchivo = entrada.readUTF();
-						long sizeArchivo = entrada.readLong();
-						File archivoSalida = new File(respaldos+"Respaldo_"+LocalDate.now()+"_"+nombreArchivo);
-						
-						try (FileOutputStream salida = new FileOutputStream(archivoSalida)){
-		                	int unByte = entrada.read();
-		                	long sizeActual = 0;
-		                	while(sizeActual < sizeArchivo && unByte != -1) {
-		                		salida.write(unByte);
-		                		sizeActual++;
-		                		unByte = entrada.read();
-		                		
-		                	}
-		                	System.out.println("Respaldo guardado: "+archivoSalida.getName());
-		                }
-					}catch (EOFException e) {
-						pasandoArchivos = false;
+			try {
+				Socket sckCliente = sckServer.accept();
+			
+				new Thread() {
+					public void run() {
+						try	(Socket sckActual = sckCliente;
+							DataInputStream entrada = new DataInputStream(sckActual.getInputStream())){
+							
+							boolean pasandoArchivos = true;
+							
+							while(pasandoArchivos) {
+								try {
+									String nombreArchivo = entrada.readUTF();
+									long sizeArchivo = entrada.readLong();
+									File archivoSalida = new File(respaldos+"Respaldo_"+LocalDate.now()+"_"+nombreArchivo);
+									
+									try (FileOutputStream salida = new FileOutputStream(archivoSalida)){
+					                	int unByte = entrada.read();
+					                	long sizeActual = 0;
+					                	while(sizeActual < sizeArchivo && unByte != -1) {
+					                		salida.write(unByte);
+					                		sizeActual++;
+					                		unByte = entrada.read();
+					                		
+					                	}
+					                	System.out.println("Respaldo guardado: "+archivoSalida.getName());
+					                }
+								}catch (EOFException e) {
+									pasandoArchivos = false;
+								}
+							}
+						}catch(IOException e) {
+							System.out.println("Error");
+						}
 					}
-				}
+				}.start();
 			}catch(IOException e) {
 				System.out.println("Error");
 			}
